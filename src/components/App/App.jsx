75d6ae36701fac7cee8a021ea2e241976844ab71
nick <ndweir13@gmail.com>
2024-10-10 10:57:16 -0500

    PizzaList and PizzaForm components created

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom/cjs/react-router-dom.min';
+import PizzaForm from '../PizzaForm/PizzaForm';
+import PizzaList from '../PizzaList/PizzaList';
+
 
 function App() {
+  const dispatch = useDispatch();
+
+  const fetchPizza = () => {
+    axios.get('/api/pizza').then(response => {
+      console.log('GET RESPONSE DATA', response.data)
+      dispatch({
+        type: 'SET_PIZZA',
+        payload: response.data
+      })
+    }).catch(err => {
+      console.error('GET ERROR', err)
+    })
+  }
+
+  useEffect(() => {
+    fetchPizza()
+  },[])
+
+  // router sketch out below
+
 
   return (
     <div className='App'>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,26 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 
 // Be sure to replace this reducer! 🙂
-const someReducer = (state = [], action) => {
+const pizzaList = (state = [], action) => {
+
+  if(action.type === 'SET_PIZZA'){
+    return action.payload
+  }
+
+  if(action.type === 'ADD_PIZZA'){
+    return [...state, action.payload]
+  }
+
+  // if(action.type === 'REMOVE_PIZZA'){
+  //   return 
+  // }
+
   return state;
 }
 
 const store = createStore(
   combineReducers({
-    someReducer, // 👈 Be sure to replace this, too!
+    pizzaList, // 👈 Be sure to replace this, too!
   }),
   applyMiddleware(logger),
 );
